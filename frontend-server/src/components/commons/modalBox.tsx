e0fd92e29cdc24724ed0843700514cd8079e052f
jatin <jatinkumar10082003@gmail.com>
2024-03-05 10:55:26 +0530

    new commit

--- frontend-server/src/components/commons/modalBox.tsx
+++ frontend-server/src/components/commons/modalBox.tsx
@@ -6,7 +6,7 @@ interface IProps {
 
 export default function ModalBox({ children, className, onClose }: IProps) {
   return (
-    <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex items-center justify-center">
+    <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 z-20 flex items-center justify-center">
       <div className={className}>
         {children}
         <button
