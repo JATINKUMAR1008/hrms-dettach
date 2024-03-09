4216c243fdf5ddf676b159decf40586f80413ce7
jatin <jatinkumar10082003@gmail.com>
2024-03-09 18:14:00 +0530

    create clustered card more responsive

--- frontend-2.0/src/components/LandingPage/clusterCard.tsx
+++ frontend-2.0/src/components/LandingPage/clusterCard.tsx
@@ -25,30 +25,30 @@ export const ClusteredCard = () => {
           $3200
         </p>
       </div>
-      <div className="rounded-lg outline outline-neutral-200 lg:ml-10 py-10 px-7">
+      <div className="rounded-lg outline outline-neutral-200 lg:ml-10 lg:py-10 py-5 lg:px-7 px-4">
         <Image src={slack_icon} alt="slack" width={50} height={50} />
-        <div className="bg-neutral-100 text-neutral-600 text-lg px-5 py-3 rounded-lg mt-5 max-w-[400px] font-medium">
+        <div className="bg-neutral-100 text-neutral-600 lg:text-lg text-sm px-5 py-3 rounded-lg mt-5 max-w-[400px] font-medium">
           <h3>Hello Yasuo</h3>
           <p className="mt-1">
             Anastasia requested 5 days of Holidays,your approval is required.
           </p>
         </div>
         <div className="w-full max-w-[400px] mt-5">
-          <div className="flex justify-between mt-3 text-lg font-medium">
+          <div className="flex justify-between mt-3 md:text-lg text-sm font-medium">
             <p className=" text-neutral-600">Employee Name</p>
             <p className=" text-neutral-900">
               {leaveExample[0]["employee name"]}
             </p>
           </div>
-          <div className="flex justify-between mt-3 text-lg font-medium">
+          <div className="flex justify-between mt-3 md:text-lg text-sm font-medium">
             <p className=" text-neutral-600">Dates</p>
             <p className=" text-neutral-900">{leaveExample[0]["dates"]}</p>
           </div>
-          <div className="flex justify-between mt-3 text-lg font-medium">
+          <div className="flex justify-between mt-3 md:text-lg text-sm font-medium">
             <p className=" text-neutral-600">Length</p>
             <p className=" text-neutral-900">{leaveExample[0]["length"]}</p>
           </div>
-          <div className="flex justify-between mt-3 text-lg font-medium">
+          <div className="flex justify-between mt-3 md:text-lg text-sm font-medium">
             <p className=" text-neutral-600">Type</p>
             <p className=" text-neutral-900">{leaveExample[0]["Type"]}</p>
           </div>
@@ -68,14 +68,14 @@ export const ClusteredCard = () => {
           Pay
         </Button>
       </div>
-      <div className="absolute px-5 rounded-md max-w-[350px] w-full py-3 flex lg:right-[150px] bg-white lg:top-12 top-[85%] outline outline-neutral-200 shadow-neutral-shadow">
-        <div className="w-14 h-14 bg-indigo-700 flex justify-center text-2xl font-extrabold items-center rounded-full">
+      <div className="absolute px-5 rounded-md max-w-[350px] lg:w-full w-fit py-3 flex lg:right-[150px] bg-white lg:top-12 top-[85%] outline outline-neutral-200 shadow-neutral-shadow">
+        <div className="lg:w-14 lg:h-14 w-10 h-10 bg-indigo-700 flex justify-center lg:text-2xl md:text-xl text-base font-extrabold items-center rounded-full">
           JK
         </div>
-        <div className="w-14 h-14 relative right-6 outline bg-green-600 flex justify-center text-2xl font-extrabold items-center rounded-full">
+        <div className="lg:w-14 lg:h-14 w-10 h-10 relative right-6 outline bg-green-600 flex justify-center lg:text-2xl md:text-xl text-base font-extrabold items-center rounded-full">
           HS
         </div>
-        <p className="text-lg font-medium text-neutral-600 break-words max-w-[200px]">
+        <p className="lg:text-lg text-sm font-medium text-neutral-600 break-words max-w-[200px]">
           Kasandra {"'"}s take home increased by{" "}
           <span className="text-used-green font-extrabold">$430</span>
         </p>
